4f2b889600ed29abd78c8b472c4f68bed5031380
Lucas Rogerio da Silva <109930651+Lucas44794@users.noreply.github.com>
2023-01-16 23:45:28 -0300

    Atualizando usando Web Scraping

--- dividendo.py
+++ dividendo.py
@@ -1,52 +1,92 @@
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+
 Codigo = str(input('Código da ação: ')).strip().upper()
-vv = str(input('Valor atual da ação: ')).replace(',', '.')
+
+while True:
+    urlErro = f"https://www.google.com/finance/quote/{Codigo}:BVMF"
+    paginaErro = requests.get(urlErro)
+    erroGeral = html.fromstring(paginaErro.content)
+    erro = erroGeral.xpath('//*[@id="yDmH0d"]/c-wiz/div/div[4]/div/div/div[2]/text()')
+    erro = str(erro).replace('["','').replace('"]','')
+    erro = str(erro).strip()
+    if erro == "We couldn't find any match for your search.":
+        Codigo = str(input('Código da ação: ')).strip().upper()
+    else:
+        break
+
+url2 = f"https://www.google.com/finance/quote/{Codigo}:BVMF"
+url = url2
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'lxml')
+list_item = soup.find('div', attrs={'class': 'YMlKec fxKbKc'})
+ap = str(list_item).replace('<div class="YMlKec fxKbKc">R$', '')
+ar = str(ap).replace('</div>', '')
+vv = str(ar).replace(',', '.')
 valorAtual = float(vv)
+
 aapt = str(input('Digite o valor aportado mensalmente: R$')).replace(',', '.')
 aporte = float(aapt)
 temp = int(input('Por quanto tempo o aporte será efetuado em anos? '))
 tempo = temp * 12
-dd = str(input('Qual a Média dos ultimos 10 dividendos distribuidos? R$')).replace(',', '.')
-mediaDividendo = float(dd)
+
+ulr3 = f"https://www.fundsexplorer.com.br/funds/{Codigo}"
+url4 = ulr3
+page2 = requests.get(url4)
+site2 = html.fromstring(page2.content)
+precoDividendo12mesestxt = site2.xpath('//*[@id="dividends"]/div/div/div[2]/div[1]/div/div[2]/table/tbody/tr[1]/td[5]/text()')
+precoDividendo12mesestxt = str(precoDividendo12mesestxt).replace('R$', '').replace(',', '.').replace('[', '').replace(']', '').replace("'",'')
+precoDividendo12mesestxt = str(precoDividendo12mesestxt).strip()
+precoDividendo12meses = float(precoDividendo12mesestxt)
+
+
+mediaDividendo = precoDividendo12meses / 12
 reinvestimentoDividendo = str(input('Haverá o reinvestimento de dividendos? [S/N]: ')).strip().upper()[0]
 while reinvestimentoDividendo not in 'SsNn':
     print('Resposta incorreta, tente novamente...')
     reinvestimentoDividendo = str(input('Haverá o reinvestimento de dividendos? [S/N]: ')).strip().upper()[0]
-mes = total = dividendoTotal = totalAcoes = acoes = Saldo = 0
-print()
-print()
-print()
 while True:
+    visualizar = str(input('Deseja ver os calculos mensalmente? [S/N]: ]')).strip().upper()[0]
+    if visualizar in 'SN':
+        break
+total = dividendoTotal = totalAcoes = acoes = Saldo = 0
+mes = 1
+print('\n \n \n')
 
-
-
+while True:
     acoes = int(aporte / valorAtual)
     Saldo += aporte % valorAtual
     total += aporte
-    if mes != 0:
+    if mes != 1:
         dividendoTotal = mediaDividendo * totalAcoes
         dividendoTotal += Saldo
         Saldo = 0
-    print(f'No mês {mes + 1} você ganhará {dividendoTotal:.2f} em dividendo.')
+    if visualizar == 'S':
+       print(f'No mês {mes} você ganhará {dividendoTotal:.2f} em dividendo.')
 
     totalAcoes += acoes
 
     if reinvestimentoDividendo in 'Ss':
         if dividendoTotal >= valorAtual:
             acoesCompradasComDividendo = int(dividendoTotal / valorAtual)
-            print(f'No mês {mes + 1} foram adquiridas {acoesCompradasComDividendo} ações compradas usando o dividendo.')
+            if visualizar == 'S':
+                print(f'Com o valor aportado, foram adquiridas {int(aporte / valorAtual)} ações.')
+                print(f'Foram adquiridas {acoesCompradasComDividendo} ações compradas usando o dividendo.')
             Saldo += dividendoTotal % valorAtual
             totalAcoes += acoesCompradasComDividendo
         elif dividendoTotal < valorAtual:
             Saldo += dividendoTotal
-        print(f'Total de ações: {totalAcoes}')
-        print()
+        if visualizar in 'S':
+            print(f'Total de ações: {totalAcoes}')
+            print()
         if mes >= tempo:
             print('-=' * 50)
             print(f'{" " * espaco}{Codigo}{" " * espaco}')
             print('-=' * 50)
             print(f'=>   Foram adquiridas {totalAcoes} ações  ')
             print(f'=>   O total aportado foi {total:.2f}')
-            print(f'=>   Saldo restante {Saldo:.2f}')
             print(f'=>   O valor total que as ações valem com o reinvestimento dos dividendos é: {float(totalAcoes * valorAtual):.2f}')
             break
 
@@ -62,5 +102,5 @@ while True:
             print('-=' * 50)
             print(f'=>   Foram adquiridas {totalAcoes} ações  ')
             print(f'=>   O total aportado foi {total:.2f}')
-            print(f'=>   Saldo restante {Saldo:.2f}')
+            break
     espaco = int((100 - len(Codigo)) / 2)
